a95c76471e1f97237d19b6d7fe884eae25fc0b83
Filip Filmar <filmil@gmail.com>
2025-11-24 01:21:44 +0000

    fix: update the vivado path

--- build/vivado/bin/proggen/main_script.tpl.sh
+++ build/vivado/bin/proggen/main_script.tpl.sh
@@ -32,6 +32,9 @@ if [[ "${gotopt2_device}" == "" ]]; then
 fi
 
 readonly _tcl_script_file="prog.tcl"
+# The root of the Vivado installation in the container's filesystem.
+readonly _vivado_version="2025.1"
+readonly _vivado_root="/opt/Xilinx/Vivado/${_vivado_version}"
 
 cat <<EOF > "${_tcl_script_file}"
 # Vivado tcl script here.
@@ -46,7 +49,6 @@ current_hw_target [get_hw_targets $gotopt2_device]
 open_hw_target
 puts "INFO: Done connecting."
 
-# Program and Refresh the XC7K325T Device
 set Device [lindex [get_hw_devices] 0]
 current_hw_device \$Device
 refresh_hw_device -update_hw_probes false \$Device
@@ -65,14 +67,14 @@ puts "INFO: Done."
 EOF
 env RUNFILES_DIR="$PWD/.." \
 "${_run_docker}" \
-    --container=xilinx-vivado:latest \
+    --container=xilinx-vivado:${_vivado_version} \
     --dir-reference=${PWD} \
     --source-dir=${PWD} \
     --mounts=/tmp/.X11-unix:/tmp/.X11-unix:ro \
     --freeargs=--net=host,-e,HOME=/work,-w,/work \
     --src-mount=/work \
-    LD_LIBRARY_PATH="/opt/Xilinx/Vivado/2023.2/lib/lnx64.o" \
-    /opt/Xilinx/Vivado/2023.2/bin/setEnvAndRunCmd.sh vivado \
+    LD_LIBRARY_PATH="${_vivado_root}/lib/lnx64.o" \
+    "${_vivado_root}/bin/setEnvAndRunCmd.sh" vivado \
     -notrace -mode batch \
     -source "/work/${_tcl_script_file}"
 
